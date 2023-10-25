10a1239711739c8196b30304719f6d013cab7b0e
beratberkayg <berat.1675@gmail.com>
2023-10-25 17:29:23 +0300

    update

--- components/movies-section/index.js
+++ components/movies-section/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const MovieSection = () => {
-  return <div>index</div>;
-};
+import styles from "./styles.module.css";
 
-export default MovieSection;
+function MoviesSection({ title, movies }) {
+  return (
+    <div className={styles.moviesSection}>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.movies}>
+        {movies.map((movie) => (
+          <div className={styles.movie} key={movie.id}>
+            <Link href={`/movie/${movie.id}`}>
+              <Image
+                fill
+                unoptimized
+                alt={movie.title}
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MoviesSection;
