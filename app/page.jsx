0dc06a7bcaba85c1aad9a29bb7d9ddcd0df52232
beratberkayg <berat.1675@gmail.com>
2023-10-25 16:22:34 +0300

    create featured comp and mocks json

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import HomeContainer from "@/containers/home";
 function HomePage() {
-  return <div>merhaba</div>;
+  return <HomeContainer />;
 }
 
 export default HomePage;
